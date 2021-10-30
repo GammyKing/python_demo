44443352ea0de422911d7af8f225fa9da47c6f90
gammyking <1106015786@qq.com>
2021-10-30 16:53:54 +0800

    函数练习

---
+++ test05.py
@@ -0,0 +1,25 @@
+# 字符串和常用数据结构
+# str1 = 'hello world!'
+# print(len(str1))
+# print(str1.capitalize())
+# print(str1.title())
+# print(str1.upper())
+# print(str1.find('or'))
+# print(str1.find('shit'))
+# print(str1.startswith('He'))
+# print(str1.startswith('hel'))
+# print(str1.center(50, '*'))
+# print(str1.rjust(50, ' '))
+# str2 = 'abc123456789'
+# print(str2.isdigit())
+# print(str2.isalpha())
+# print(str2.isalnum())
+# str3 = '  jackfrued@126.com '
+# print(str3)
+# # 获得字符串修剪左右两侧空格之后的拷贝
+# print(str3.strip())
+# a, b = 5, 10
+# print(f'{a} * {b} = {a*b}')
+
+# 使用列表
+list1 = [1, 3, 5, 7, 100]
