7e7a0d0cf924939aa7519264fed596a7e2810791
gammyking <1106015786@qq.com>
2021-11-17 19:46:30 +0800

    面向对象进阶

--- test/test6.py
+++ test/test6.py
@@ -128,56 +128,140 @@
 #     main()
 
 # 定义一个类描述平面上的点并提供移动点和计算到另一个点距离的方法
-from math import sqrt
-
-
-# 描述平面上两点之间的距离的类
-
-class Point(object):
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def move_to(self, x, y):
-        """
-        移动到的点
-        :param x:
-        :param y:
-        :return:
-        """
-        self.x = x
-        self.y = y
-
-    def move_by(self, dx, dy):
-        """
-        移动的横竖增量
-        :param dx:
-        :param dy:
-        :return:
-        """
-        self.x += dx
-        self.y += dy
-
-    def distinct_to(self, other):
-        """
-        与另外一个点的横竖差距
-        :param other:
-        :return:
-        """
-        dx = self.x - other.x
-        dy = self.y - other.y
-        return sqrt(dx ** 2 + dy ** 2)
-
-    def __str__(self):
-        return '(%s,%s)' % (str(self.x), str(self.y))
-
-
-def main():
-    p1 = Point(1, 2)
-    p2 = Point(0, 0)
-    p2.move_by(2, 3)
-    print(p1.distinct_to(p2))
-
-
-if __name__ == '__main__':
-    main()
+# from math import sqrt
+#
+#
+# # 描述平面上两点之间的距离的类
+#
+# class Point(object):
+#     def __init__(self, x=0, y=0):
+#         self.x = x
+#         self.y = y
+#
+#     def move_to(self, x, y):
+#         """
+#         移动到的点
+#         :param x:
+#         :param y:
+#         :return:
+#         """
+#         self.x = x
+#         self.y = y
+#
+#     def move_by(self, dx, dy):
+#         """
+#         移动的横竖增量
+#         :param dx:
+#         :param dy:
+#         :return:
+#         """
+#         self.x += dx
+#         self.y += dy
+#
+#     def distinct_to(self, other):
+#         """
+#         与另外一个点的横竖差距
+#         :param other:
+#         :return:
+#         """
+#         dx = self.x - other.x
+#         dy = self.y - other.y
+#         return sqrt(dx ** 2 + dy ** 2)
+#
+#     def __str__(self):
+#         return '(%s,%s)' % (str(self.x), str(self.y))
+#
+#
+# def main():
+#     p1 = Point(1, 2)
+#     p2 = Point(0, 0)
+#     p2.move_by(2, 3)
+#     print(p1.distinct_to(p2))
+#
+#
+# if __name__ == '__main__':
+#     main()
+# class Person(object):
+#     # 限定Person对象只能绑定_name,_age,_gender
+#     __slots__ = ('_name', '_age', '_gender')
+#
+#     def __init__(self, name, age):
+#         self._name = name
+#         self._age = age
+#
+#     # 访问器 -getter方法
+#     @property
+#     def name(self):
+#         return self._name
+#
+#     # 访问器 --setter方法
+#     @property
+#     def age(self):
+#         return self._age
+#
+#     # 修改器 --setter方法
+#     @age.setter
+#     def age(self, age):
+#         self._age = age
+#
+#     @name.setter
+#     def name(self, name):
+#         self._name = name
+#
+#     def play(self):
+#         if self._age <= 16:
+#             print("%s正在玩儿飞行棋。" % self._name)
+#         else:
+#             print("%s正在玩儿斗地主。" % self._name)
+#
+#
+# def main():
+#     person = Person('www', 12)
+#     person.play()
+#     person.name = 'qqq'
+#     person.age = 22
+#     person.play()
+#
+#
+# if __name__ == '__main__':
+#     main()
+# 构建三角形
+# from math import sqrt
+#
+#
+# class Triangle(object):
+#     def __init__(self, a, b, c):
+#         self._a = a
+#         self._b = b
+#         self._c = c
+#
+#     @staticmethod
+#     def isvalid(a, b, c):
+#         return a + b > c and a + c > b and b + c > a
+#
+#     # 计算周长
+#     def circle(self):
+#         return self._a + self._b + self._c
+#
+#     # 计算面积
+#     def area(self):
+#         half = 1 / 2 * self.circle()
+#         return sqrt(half * (half - self._a) * (half - self._b) * (half - self._c))
+#
+#
+# def main():
+#     a, b, c = 3, 4, 5
+#     # 静态方法和类方法都是通过给类发送消息来调用的
+#     if Triangle.isvalid(a, b, c):
+#         t = Triangle(a, b, c)
+#         # 也可以通过类发消息来调用对象方法但是要传入接受消息的对象作为参数
+#         # print（Triangle.circle(t))
+#         print(Triangle.circle(t))
+#         print(t.circle())
+#         print(t.area())
+#     else:
+#         print("Can not make it as Triangle!")
+#
+#
+# if __name__ == '__main__':
+#     main()
