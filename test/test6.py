09e1acfb314767852fe84d320e446f3d3742f446
gammyking <1106015786@qq.com>
2021-11-17 20:36:43 +0800

    面向对象进阶

--- test/test6.py
+++ test/test6.py
@@ -265,3 +265,178 @@
 #
 # if __name__ == '__main__':
 #     main()
+
+# from time import time, localtime, sleep
+#
+#
+# class Clock(object):
+#     """数字时钟"""
+#
+#     def __init__(self, hour=0, minute=0, second=0):
+#         self._hour = hour
+#         self._minute = minute
+#         self._second = second
+#
+#     @classmethod
+#     def now(cls):
+#         ctime = localtime(time())
+#         return cls(ctime.tm_hour, ctime.tm_min, ctime.tm_sec)
+#
+#     def run(self):
+#         """走字"""
+#         self._second += 1
+#         if self._second == 60:
+#             self._second = 0
+#             self._minute += 1
+#             if self._minute == 60:
+#                 self._minute = 0
+#                 self._hour += 1
+#                 if self._hour == 24:
+#                     self._hour = 0
+#
+#     def show(self):
+#         """显示时间"""
+#         return '%02d:%02d:%02d' % \
+#                (self._hour, self._minute, self._second)
+#
+#
+# def main():
+#     # 通过类方法创建对象并获取系统时间
+#     clock = Clock.now()
+#     while True:
+#         print(clock.show())
+#         sleep(1)
+#         clock.run()
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+# 类之间的关系
+# 简单来说，类之间的关系有三种：is-a/has-a/use-a
+# is-a关系也叫继承或泛化。例如学生和人
+# has-a通常称之为关联。比如部门和员工之间的关系，汽车和引擎之间的关系都属于关联关系；
+# 关联关系如果是整体和部分的，我们称之为 聚合关系
+# 如果整体进一步负责了部分的生命周期（整体部分是不可分割的，同时存在也同时消亡。）那么这种就是最强的关联关系
+# 我们称之为合成关系。
+# use-a通常称为依赖关系，比如司机有一个驾驶的行为（方法），其中（的参数） 使用到了汽车，那么司机和汽车的关系就是依赖关系。
+
+# 继承与多态
+class Person(object):
+    """human"""
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    def play(self):
+        print('%s正在愉快的玩耍.' % self._name)
+
+    def watch_av(self):
+        if self._age > 16:
+            print('%s正在观看爱情动作片.' % self._name)
+        else:
+            print('%s只能观看《熊出没》.' % self._name)
+
+
+# 学生 继承 人
+class Student(Person):
+    """学生"""
+
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        self._grade = grade
+
+    @property
+    def grade(self):
+        return self._grade
+
+    @grade.setter
+    def grade(self, grade):
+        self._grade = grade
+
+    # 新建了方法，并且多加了功能
+    def study(self, course):
+        print('%s的%s正在学习%s.' % (self._grade, self._name, course))
+
+
+class Teacher(Person):
+    """老师"""
+
+    def __init__(self, name, age, title):
+        super().__init__(name, age)
+        self._title = title
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+
+    def teach(self, course):
+        print('%s%s正在讲%s.' % (self._name, self._title, course))
+
+
+def main():
+    stu = Student('王大锤', 15, '初三')
+    stu.study('数学')
+    stu.watch_av()
+    t = Teacher('骆昊', 38, '砖家')
+    t.teach('Python程序设计')
+    t.watch_av()
+
+
+# if __name__ == '__main__':
+#     main()
+# 子类重写父类方法
+from abc import ABCMeta, abstractmethod
+
+
+class Pet(object, metaclass=ABCMeta):
+    """宠物"""
+
+    def __init__(self, nickname):
+        self._nickname = nickname
+
+    @abstractmethod
+    def make_voice(self):
+        """make sound"""
+        pass
+
+
+class Dog(Pet):
+    """狗"""
+
+    def make_voice(self):
+        print('%s:汪汪汪...' % self._nickname)
+
+
+class Cat(Pet):
+    """猫"""
+
+    def make_voice(self):
+        print('%s: 喵...喵...' % self._nickname)
+
+
+def main():
+    pets = [Dog('旺财'), Cat('凯蒂'), Dog('大黄')]
+    for pet in pets:
+        pet.make_voice()
+
+
+if __name__ == '__main__':
+    main()
